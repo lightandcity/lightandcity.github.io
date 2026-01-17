5b1f49fd364226319217ea0aca4c52bfda4431bc
Ingyeom Kwon <lightandcity.co@gmail.com>
2026-01-17 14:34:41 +0900

    update

---
+++ repl_new.py
@@ -0,0 +1,80 @@
+import os
+import shutil
+
+# ===== 설정부 =====
+ROOT_DIR = r"C:\Users\user\Desktop\빛과도시\홈페이지\latest\lightandcity.github.io"
+TARGET_EXTENSIONS = (".html", ".js", ".css")
+
+OLD_BLOCK = """
+        <li class="depth-02" data-code="m20230323d6f698cbf5bb0" style="">
+         <a class="" data-has_child="N" data-is_folder_menu="N" data-url="41" href="41.html" onclick="" tabindex="-1">
+          <span class="plain_name" data-hover="">
+           업무실적
+          </span>
+          <span class="_toggle_btn toggle-btn">
+          </span>
+         </a>
+        </li>"""
+NEW_BLOCK = """
+        <li class="depth-02" data-code="m20230323d6f698cbf5bb0" style="">
+         <a class="" data-has_child="N" data-is_folder_menu="N" data-url="41" href="41.html" onclick="" tabindex="-1">
+          <span class="plain_name" data-hover="">
+           업무실적
+          </span>
+          <span class="_toggle_btn toggle-btn">
+          </span>
+         </a>
+        </li>        
+        <li class="depth-02" data-code="m20230323d6f698cbf5bb0" style="">
+         <a class="" data-has_child="N" data-is_folder_menu="N" data-url="61" href="61.html" onclick="" tabindex="-1">
+          <span class="plain_name" data-hover="">
+           의뢰사례
+          </span>
+          <span class="_toggle_btn toggle-btn">
+          </span>
+         </a>
+        </li>"""
+
+BACKUP = True
+# =================
+
+
+def replace_block_in_file(file_path, old_block, new_block, backup=True):
+    with open(file_path, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    if old_block not in content:
+        return
+
+    if backup:
+        shutil.copy(file_path, file_path + ".bak")
+
+    content = content.replace(old_block, new_block)
+
+    with open(file_path, "w", encoding="utf-8") as f:
+        f.write(content)
+
+    print(f"[REPLACED] {file_path}")
+
+
+def replace_block_in_folder(root_dir, old_block, new_block, backup=True):
+    for root, _, files in os.walk(root_dir):
+        for file in files:
+            if file.endswith(TARGET_EXTENSIONS):
+                file_path = os.path.join(root, file)
+                replace_block_in_file(
+                    file_path,
+                    old_block,
+                    new_block,
+                    backup
+                )
+
+
+if __name__ == "__main__":
+    replace_block_in_folder(
+        ROOT_DIR,
+        OLD_BLOCK,
+        NEW_BLOCK,
+        BACKUP
+    )
+    print("✅ 블록 치환 완료")
